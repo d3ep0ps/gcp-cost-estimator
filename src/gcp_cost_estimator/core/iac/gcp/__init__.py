8c58f58adc5c943e9017124e37ab6fcf0ee72fa4
Vitaliy Zhhuta <zhhuta@gmail.com>
2026-06-18 10:43:28 +0300

    feat(iac): migrate google_storage_bucket to modular storage parser

--- src/gcp_cost_estimator/core/iac/gcp/__init__.py
+++ src/gcp_cost_estimator/core/iac/gcp/__init__.py
@@ -8,6 +8,7 @@ from gcp_cost_estimator.core.iac.gcp.compute import (
 )
 from gcp_cost_estimator.core.iac.gcp.context import ParserContext
 from gcp_cost_estimator.core.iac.gcp.sql import parse_sql_database_instance
+from gcp_cost_estimator.core.iac.gcp.storage import parse_storage_bucket
 from gcp_cost_estimator.core.model import Resource
 
 ParserFunc = Callable[[str, ParserContext, dict[str, str]], Resource]
@@ -16,4 +17,5 @@ RESOURCE_TYPE_MAP: dict[str, ParserFunc] = {
     "google_compute_instance": parse_compute_instance,
     "google_compute_disk": parse_compute_disk,
     "google_sql_database_instance": parse_sql_database_instance,
+    "google_storage_bucket": parse_storage_bucket,
 }
