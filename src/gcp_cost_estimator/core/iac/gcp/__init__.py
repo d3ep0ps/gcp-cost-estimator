a1c2a2a86840fa5a8614199a86ca519f5db7c60b
Vitaliy Zhhuta <zhhuta@gmail.com>
2026-06-18 10:46:13 +0300

    feat(iac): migrate Cloud Run, Cloud Functions, and App Engine to modular serverless parser

--- src/gcp_cost_estimator/core/iac/gcp/__init__.py
+++ src/gcp_cost_estimator/core/iac/gcp/__init__.py
@@ -12,6 +12,15 @@ from gcp_cost_estimator.core.iac.gcp.container import (
     parse_container_node_pool,
 )
 from gcp_cost_estimator.core.iac.gcp.context import ParserContext
+from gcp_cost_estimator.core.iac.gcp.serverless import (
+    parse_app_engine_application,
+    parse_app_engine_flexible_version,
+    parse_app_engine_standard_version,
+    parse_cloud_run_job,
+    parse_cloud_run_service,
+    parse_cloudfunctions2_function,
+    parse_cloudfunctions_function,
+)
 from gcp_cost_estimator.core.iac.gcp.sql import parse_sql_database_instance
 from gcp_cost_estimator.core.iac.gcp.storage import parse_storage_bucket
 from gcp_cost_estimator.core.model import Resource
@@ -26,4 +35,11 @@ RESOURCE_TYPE_MAP: dict[str, ParserFunc] = {
     "google_container_cluster": parse_container_cluster,
     "google_container_node_pool": parse_container_node_pool,
     "google_bigquery_dataset": parse_bigquery_dataset,
+    "google_cloud_run_v2_service": parse_cloud_run_service,
+    "google_cloud_run_v2_job": parse_cloud_run_job,
+    "google_cloudfunctions_function": parse_cloudfunctions_function,
+    "google_cloudfunctions2_function": parse_cloudfunctions2_function,
+    "google_app_engine_standard_app_version": parse_app_engine_standard_version,
+    "google_app_engine_flexible_app_version": parse_app_engine_flexible_version,
+    "google_app_engine_application": parse_app_engine_application,
 }
